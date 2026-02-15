95d851e1ac36533386797da28c2a50ff6a8405f1
NTeditor <nteditor4@gmail.com>
2026-02-15 20:05:41 +0300

    global: fish: refactor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,11 @@
+set -gx EDITOR "nvim"
+set -gx PAGER "bat"
+
+alias ls eza
+alias fb fastboot
+complete -c ls -w eza
+complete -c fb -w fastboot
+
 if status is-login
   if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
     exec start-hyprland &>>~/.cache/hyprland.log
