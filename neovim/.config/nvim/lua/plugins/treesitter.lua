befab73e40d54f8ecb7ab42992a6dbc6536f48b8
NTeditor <nteditor4@gmail.com>
2026-02-16 19:44:04 +0300

    neovim: update tree-sitter

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,29 @@
+---@class MyTreesitterOpts
+---@field languages string[]
+
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
+	---@type MyTreesitterOpts
 	opts = {
-		ensure_installed = { "c", "lua", "rust", "go", "bash", "sh", "markdown", "markdown_inline", "nu" },
-		auto_install = true,
-		sync_install = true,
-		highlight = {
-			enable = true,
-		},
+		languages = { "lua", "c", "cpp", "html", "css", "rust", "json", "json5", "bash", "zsh", "fish" },
 	},
+
+	---@param opts MyTreesitterOpts
+	config = function(_, opts)
+		require("nvim-treesitter").install(opts.languages):wait(60000)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = (function(list)
+				local languages = vim.deepcopy(list)
+				if vim.tbl_contains(languages, "json") then
+					table.insert(languages, "jsonc")
+				end
+				return languages
+			end)(opts.languages),
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
